dc95a5a47a5e5b4088eb86e4b73cef98f40d99d9
sepehr ansaripour <pehrans@gmail.com>
2017-02-09 22:50:01 -0500

    Added basic animation controllers and animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		if (horizontal > 0) {
+			animator.SetBool ("Walk", true);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (2.0f, 0);
+
+		} else if (horizontal == 0) {
+			animator.SetBool ("Walk", false);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		}
+	}
+}
