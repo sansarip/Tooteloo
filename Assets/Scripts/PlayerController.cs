d1c40db8ece9268f4f11beb6139591c75185bac6
sepehr ansaripour <pehrans@gmail.com>
2017-02-10 17:29:47 -0500

    Basic key-combo movement system added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	private int comboPosition;
+	public float speed;
+	public int health;
+	private char[] keyList;
 	private Animator animator;
+	private int timewalking;
+
 
 	// Use this for initialization
 	void Start () {
-		animator = this.GetComponent<Animator> ();	
+		animator = this.GetComponent<Animator> ();
+		keyList = new char[4];
+		comboPosition = 0;
+		timewalking = 0;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		float horizontal = Input.GetAxis("Horizontal");
-		if (horizontal > 0) {
+		if (Input.GetKeyUp ("q")) {
+			int index = comboPosition % 4;
+			keyList [index] = 'q';
+			comboPosition++;
+			Debug.Log ("Q\n");
+			Debug.Log ("comboPosition%4: " + comboPosition % 4 + "\n");
+		}
+		if (Input.GetKeyUp ("w")) {
+			int index = comboPosition % 4;
+			keyList [index] = 'w';
+			comboPosition++;
+			Debug.Log ("W\n");
+			Debug.Log ("comboPosition%4: " + comboPosition % 4 + "\n");
+		}
+		if (Input.GetKeyUp ("e")) {
+			int index = comboPosition % 4;
+			keyList [index] = 'e';
+			comboPosition++;
+			Debug.Log ("E\n");
+			Debug.Log ("comboPosition%4: " + comboPosition % 4 + "\n");
+		}
+		if (Input.GetKeyUp ("r")) {
+			int index = comboPosition % 4;
+			keyList [index] = 'r';
+			comboPosition++;
+			Debug.Log ("R\n");
+			Debug.Log ("comboPosition%4: " + comboPosition % 4 + "\n");
+		}
+		Debug.Log ("keyList: " + keyList[0] + "|" + keyList[1] + "|" + keyList[2] + "|" + keyList[3] + "\n");
+		if (keyList[0] == 'q' && keyList[1] == 'w' && keyList[2] == 'e' && keyList[3] == 'r') {
 			animator.SetBool ("Walk", true);
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (2.0f, 0);
-
-		} else if (horizontal == 0) {
+			Debug.Log ("SUCCESS\n");
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, 0);
+			keyList = new char[4];
+		}
+		if (animator.GetBool ("Walk") && timewalking == 0 || timewalking % 100 != 0) {
+			timewalking += (int) (Time.deltaTime * 100);
+			Debug.Log ("Timewalking: " + timewalking + "\n");
+		} else if (timewalking != 0 && timewalking % 2 == 0) {
 			animator.SetBool ("Walk", false);
+			timewalking = 0;
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		}
+		if (comboPosition >= 0x7FFFFFFC) {																		// reset comboPosition if it nears 32 bit bounds
+			Debug.Log ("RESET\n");					
+			comboPosition %= 4;
 		}
 	}
 }
