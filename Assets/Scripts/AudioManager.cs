30a068ade6714ca928c018482e17e56741a80ea4
sepehr ansaripour <pehrans@gmail.com>
2017-02-14 01:39:49 -0500

    Added basic 120bpm music and music managing script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = (AudioSource) GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
