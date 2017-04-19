3590e85d91dd0ecea6276ddd3b0a20c92a815bfe
Dong Woo Jung <Dwoo@MacBook-Pro-51.byod.gmu.edu>
2017-04-19 14:20:08 -0400

    balanced rhythm system

--- Assets/Scripts/Rhythm_System.cs
+++ Assets/Scripts/Rhythm_System.cs
@@ -9,17 +9,13 @@ public class Rhythm_System : MonoBehaviour
 	//Can  be changed to numbers divisible by 4 to increase or decrease 
 	//the length of the buffer.
 	public Transform clockPos;
-	int division = 4;
+	int division = 8;
 	int count = 0;
 	public static bool accept = false;
-	private GameObject clock;
-	private SpriteRenderer sRender;
 	public Rigidbody projectile;
 
 	void Start()
 	{
-		clock = (GameObject) GameObject.Find ("Clock"); //
-		sRender = clock.GetComponent<SpriteRenderer> ();
 		//0.5 is 120 beats per minute
 		InvokeRepeating("getInput", 0f, 0.5f/division);
 
@@ -29,16 +25,16 @@ public class Rhythm_System : MonoBehaviour
 	{
 		count++;
 		//start of buffer time
-		if (count == division - 2) {
-			sRender.enabled = true; //
+		if (count == division - 3) {
 			accept = true;
+		}
+		if (count == 3) {
+			accept = false;
 		}
 
 		//resets after buffer time has passed
 		if (count > division) {
 			count = 1;
-			sRender.enabled = false; //
-			accept = false;
 		}
 	}
 }
